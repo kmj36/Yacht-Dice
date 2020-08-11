0a4bdb6e6ff2b359f71cd8b032fff9c39084452a
kmj36 <66502982+kmj36@users.noreply.github.com>
2020-08-11 22:41:13 +0900

    Update source.cpp
    주석 처리 및 디버그 주석 제거

--- Yacht-Dice/source.cpp
+++ Yacht-Dice/source.cpp
@@ -5,47 +5,48 @@
 #include <mmsystem.h>
 #pragma comment(lib,"winmm.lib")
 #pragma warning(disable : 4996)
-#define BONUS 40
+// i,j,k ... 등은 다 인덱스 변수임.
+#define BONUS 40 // 보조 합계가 63 이상일 경우 + BONUS
 
-int compare(const void* a, const void* b) // 비교 오픈 소스
+int compare(const void* a, const void* b) // 비교 오픈 소스 이용함
 {
-	return *(int*)a - *(int*)b;
+	return *(int*)a - *(int*)b; // 역참조 후 a-b 후 반환
 }
 
-typedef struct high_sections
+typedef struct high_sections //상단 족보
 {
-	int arr[6];
-	char flag[6];
+	int arr[6]; // 점수 배열
+	char flag[6]; // 점수 중복 쓰기 방지
 }high;
 
-typedef struct low_sections
+typedef struct low_sections // 하단 족보
 {
-	int arr[6];
-	char flag[6];
+	int arr[6]; // 점수 배열
+	char flag[6]; // 점수 중복 쓰기 방지
 }low;
 
-int print_mainmenu(void);
-int print_turn(void);
-void play(int, high*, low*, high*, low*);
-void dice_and_select(int[],int, high*, low*);
-void reset_dice(int []);
-void print_Dnm_score(int[], int[], high*, low*);
-void selects(int[],high*, low*);
-void reset_structer(high*, low*, high*, low*);
-void result(int, int);
-void rule(void);
-void using_music(void);
+int print_mainmenu(void); // 메인 메뉴
+int print_turn(void); // 가위바위보로 턴 정하기
+void play(int, high*, low*, high*, low*); // 플레이
+void dice_and_select(int[],int, high*, low*); // 주사위 굴리기 밑 선택
+void reset_dice(int []); // 주사위 0으로 초기화
+void print_Dnm_score(int[], int[], high*, low*); // 동적 점수판 출력
+void selects(int[],high*, low*); // 점수 선택
+void reset_structer(high*, low*, high*, low*); // 구조체 숫자 0, 알파벳 O으로 초기화
+void result(int, int); // 마지막 비교 후 결과 출력
+void rule(void); // 규칙 출력
+void using_music(void); // 사용 음악 출력
 
 int main(void) // main
 {
-	system("title Yacht Dice");
-	PlaySound(TEXT("bgm.wav"), 0, SND_FILENAME | SND_ASYNC | SND_LOOP);
-	high *p1_H_s = (high*)malloc(sizeof(high)), *p2_H_s = (high*)malloc(sizeof(high));
-	low *p1_L_s = (low*)malloc(sizeof(low)) , *p2_L_s = (low*)malloc(sizeof(low));
-	int turn, sel=4;
+	system("title Yacht Dice"); // exe 제목 Yacht Dice로 지정
+	PlaySound(TEXT("bgm.wav"), 0, SND_FILENAME | SND_ASYNC | SND_LOOP); // 음악 출력, 무한 반복
+	high *p1_H_s = (high*)malloc(sizeof(high)), *p2_H_s = (high*)malloc(sizeof(high)); // 구조체 포인터 동적할당
+	low *p1_L_s = (low*)malloc(sizeof(low)) , *p2_L_s = (low*)malloc(sizeof(low)); // 구조체 동적할당
+	int turn, sel=4; // 턴 및 메인 메뉴 선택 초기화 4 = 프로그램 종료
 
 	reset_structer(p1_H_s, p1_L_s, p2_H_s, p2_L_s);
-	while (sel != 1)
+	while (sel != 1) // 1이면 가위바위보로 이동
 	{
 		sel = print_mainmenu();
 		if (sel == 2)
@@ -55,17 +56,18 @@ int main(void) // main
 		else if (sel == 4)
 			return 0;
 	}
-	system("cls");
+	system("cls"); // 화면 지우기
 
-	turn = print_turn();
+	turn = print_turn(); // 가위바위보 후 턴 반환
 	
-	if (turn % 2 == 0)
+	if (turn % 2 == 0) // 턴에 따라 플레이어 선택
 		printf("[플레이어 1] 부터 시작합니다.\n");
 	else
 		printf("[플레이어 2] 부터 시작합니다.\n");
 
 	PlaySound(NULL, 0, 0);
 
+	// 로딩하는 것 같은 화면 효과
 	Sleep(2000);
 	printf(" .");
 	Sleep(1000);
@@ -81,9 +83,10 @@ int main(void) // main
 
 	PlaySound(TEXT("bgm2.wav"), 0, SND_FILENAME | SND_ASYNC | SND_LOOP);
 
-	play(turn, p1_H_s, p1_L_s, p2_H_s, p2_L_s);
+	play(turn, p1_H_s, p1_L_s, p2_H_s, p2_L_s); // 플레이
 
 	PlaySound(NULL, 0, 0);
+	// 동적할당 반환
 	free(p1_H_s);
 	free(p1_L_s);
 	free(p2_H_s);
@@ -93,13 +96,13 @@ int main(void) // main
 
 void using_music(void)
 {
-	system("mode con cols=48 lines=36");
-	printf("[MAIN MENU] Kevin MacLeod - Samba Isobel\n");
+	system("mode con cols=48 lines=36"); // 화면 크기 변경
+	printf("[MAIN MENU] Kevin MacLeod - Samba Isobel\n"); // 유튜브 무료음원 라이브러리에서 구함.
 	printf("[GAME OST] Kevin MacLeod ~ Apero Hour\n");
 	system("pause");
 }
 
-void rule(void)
+void rule(void) // 규칙 설명 출력
 {
 	system("mode con cols=80 lines=50");
 	int i;
@@ -161,10 +164,10 @@ void rule(void)
 
 void result(int p1, int p2)
 {
-	FILE* fp = fopen("lasttime.txt", "w");
-	time_t T = time(NULL);
-	struct tm tm = *localtime(&T);
-	if (p1 > p2)
+	FILE* fp = fopen("lasttime.txt", "w"); // 마지막 플레이한 시간 파일 쓰기을 위해 fp 열기
+	time_t T = time(NULL); // 시간을 받기위해 time_t 구조체 T 선언
+	struct tm tm = *localtime(&T); // 현재 지역 시간 받기
+	if (p1 > p2) // 승패 받기
 	{
 		system("cls");
 		printf("[플레이어 1] 승리!!!");
@@ -174,12 +177,12 @@ void result(int p1, int p2)
 		system("cls");
 		printf("[플레이어 2] 승리!!!");
 	}
-	fprintf(fp, "%d-%d-%d %d:%d:%d", tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-	fclose(fp);
+	fprintf(fp, "%d-%d-%d %d:%d:%d", tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec); // 파일 쓰기
+	fclose(fp); // 파일 닫기
 	system("pause");
 }
 
-void reset_structer(high* p1_H_s, low* p1_L_s, high* p2_H_s, low* p2_L_s)
+void reset_structer(high* p1_H_s, low* p1_L_s, high* p2_H_s, low* p2_L_s) // 상단, 하단 구조체 초기화
 {
 	int i;
 	for (i = 0; i < 6; i++)
@@ -195,7 +198,7 @@ void reset_structer(high* p1_H_s, low* p1_L_s, high* p2_H_s, low* p2_L_s)
 	}
 }
 
-void selects(int score[], high * hi, low * lo)
+void selects(int score[], high * hi, low * lo) // 점수 선택
 {
 	int i, j, sel;
 	system("cls");
@@ -232,24 +235,24 @@ void selects(int score[], high * hi, low * lo)
 	{
 		printf("[점수 선택]: ");
 		scanf("%d", &sel);
-		if (sel < 1 || sel > 12)
+		if (sel < 1 || sel > 12) // 범위 벗어남 방지
 		{
 			printf("범위를 벗어났습니다. 다시 선택하여주세요.\n");
-			continue;
+			continue; // 다시 입력 받기 위해 돌아가기
 		}
-		sel -= 1;
-		if (sel < 6)
+		sel -= 1; // 인덱스로 사용하기 위해 선택 -1
+		if (sel < 6) // 상단, 하단 구별 [상단]
 		{
 			if (hi->flag[sel] == 'X')
 			{
 				printf("중복할 수 없습니다. 다시 선택하여주세요.\n");
 				continue;
 			}
-			hi->arr[sel] = score[sel];
-			hi->flag[sel] = 'X';
-			return;
+			hi->arr[sel] = score[sel]; // 점수 배열 쓰기
+			hi->flag[sel] = 'X'; // 중복 쓰기 방지을 위해 'X' 쓰기
+			return; // 함수 탈출
 		}
-		else
+		else //[하단]
 		{
 			if (lo->flag[sel-6] == 'X')
 			{
@@ -263,14 +266,14 @@ void selects(int score[], high * hi, low * lo)
 	}
 }
 
-void reset_dice(int dice[])
+void reset_dice(int dice[]) // 주사위 배열 초기화
 {
 	int i;
 	for (i = 0; i < 5; i++)
 		dice[i] = 0;
 }
 
-void print_Dnm_score(int score[], int temp[], high * hi, low * lo)
+void print_Dnm_score(int score[], int temp[], high * hi, low * lo) // 동적 점수 출력
 {
 	int cpy[5], i, j;
 	for (i = 0; i < 5; i++)
@@ -355,49 +358,49 @@ void print_Dnm_score(int score[], int temp[], high * hi, low * lo)
 
 void dice_and_select(int temp[],int last, high* hi, low* lo) // 주사위 와 선택(함수 이용) 모두 담당. 주 로직
 {
-	int dice[5] = { 0 }, i, sel, tmp;
-	int score[12];
-	for(;last >= 0;last--)
+	int dice[5] = { 0 }, i, sel, tmp; // 주사위 배열, 선택, 스왑을 위한 변수
+	int score[12]; // 점수 배열
+	for(;last >= 0;last--) // 턴 0까지 -1 탈출
 	{
 		for (i = 0; i < 5; i++)
-			dice[i] = rand() % 6 + 1;
+			dice[i] = rand() % 6 + 1; // 주사위 무작위 숫자 쓰기
 		for (i = 0; i < 5; i++)
 		{
-			if(temp[i] != 0)
-				dice[i] = 0;
+			if(temp[i] != 0) // 이미 선택한 숫자의 주사위를 제외하기 위해
+				dice[i] = 0; // 해당 주사위를 0으로 바꿈
 		}
 		while (1)
 		{
 			system("cls");
-			print_Dnm_score(score, temp, hi, lo);
+			print_Dnm_score(score, temp, hi, lo); // 동적 점수판 출력
 			printf("주사위 결과\n\n");
 			printf("[주사위]:");
 			for (i = 0; i < 5; i++)
-				printf(" %d", dice[i]);
+				printf(" %d", dice[i]); // 주사위 출력
 			printf("\n         ↓↓↓↓↓\n");
 			printf("[ 선택 ]:");
 			for (i = 0; i < 5; i++)
-				printf(" %d", temp[i]);
+				printf(" %d", temp[i]); // 플레이어의 선택한 주사위
 			printf("\n[0 = 다시 돌리기, 9 = 점수 선택] 주사위 %d번 남음\n", last);
 			printf("[주사위 선택]: ");
 			scanf("%d", &sel);
 			if (sel == 9 || last == 0)
 			{
 				for (i = 0; i < 5; i++)
-					if (temp[i] == 0)
+					if (temp[i] == 0) // 모든 주사위를 선택하지 않고 넘어갔을때
 					{
-						tmp = temp[i];
+						tmp = temp[i]; // 스왑으로 자동 넣기
 						temp[i] = dice[i];
 						dice[i] = tmp;
 					}
 				system("cls");
-				print_Dnm_score(score, temp, hi, lo);
-				selects(score, hi, lo);
+				print_Dnm_score(score, temp, hi, lo); // 점수 재 반영을 위해 한번더 점수판 출력
+				selects(score, hi, lo); // 점수 선택
 				return;
 			}
-			else if (sel > 5 || sel < 1)
+			else if (sel > 5 || sel < 1) // 1 ~ 5 를 벗어나면 주사위 다시 돌리기로 인식
 				break;
-			else
+			else // 1~5 에 따라 스왑
 			{
 				tmp = temp[sel - 1];
 				temp[sel - 1] = dice[sel - 1];
@@ -440,14 +443,14 @@ int print_turn(void) // 턴 정하는 보조 로직
 	}
 	while (1)
 	{
-		srand((unsigned int)time(NULL));
-		ans = rand() % 3 + 1;
+		srand((unsigned int)time(NULL)); // 시간에 따라 rand의 값 변경
+		ans = rand() % 3 + 1; // 시간에 따른 무작위 1 ~ 3 중 값 하나 받음
 		printf("[입력]: ");
-		scanf("%d", &res);
+		scanf("%d", &res); // 가위, 바위, 보
 		system("cls");
-		switch (res)
+		switch (res) // switch 문을 이용
 		{
-		case 1:
+		case 1: // 가위
 			if (ans == 1)
 			{
 				printf("[입력]: 가위\n");
@@ -466,7 +469,7 @@ int print_turn(void) // 턴 정하는 보조 로직
 				return 0;
 			}
 			break;
-		case 2:
+		case 2: // 바위
 			if (ans == 1)
 			{
 				printf("[입력]: 바위\n");
@@ -485,7 +488,7 @@ int print_turn(void) // 턴 정하는 보조 로직
 				return 1;
 			}
 			break;
-		case 3:
+		case 3: // 보
 			if (ans == 1)
 			{
 				printf("[입력]: 보\n");
@@ -512,10 +515,10 @@ int print_turn(void) // 턴 정하는 보조 로직
 int print_mainmenu(void) // 메인 메뉴 담당
 {
 	int i, sel;
-	char* timpe = (char*)malloc(sizeof(char) * 50);
-	FILE* fp = fopen("lasttime.txt", "r");
-	fscanf(fp,"%[^\n]s", timpe);
-	fclose(fp);
+	char* timpe = (char*)malloc(sizeof(char) * 50); // 시간을 받을 배열 동적할당
+	FILE* fp = fopen("lasttime.txt", "r"); // 마지막으로 플레이한 시간 가져옴
+	fscanf(fp,"%[^\n]s", timpe); // char * timpe에 마지막으로 플레이한 시간 넣음
+	fclose(fp); // 파일 닫기
 	system("mode con cols=48 lines=36");
 	for (i = 0; i < 36; i++)
 	{
@@ -525,8 +528,8 @@ int print_mainmenu(void) // 메인 메뉴 담당
 		}
 		else if (i == 6)
 		{
-			printf("[마지막으로 플레이 한 시간: %s]\n", timpe);
-			free(timpe);
+			printf("[마지막으로 플레이 한 시간: %s]\n", timpe); // 마지막으로 플레이한 시간 출력
+			free(timpe); // 동적할당 해제
 		}
 		else if (i == 12)
 		{
@@ -551,15 +554,17 @@ int print_mainmenu(void) // 메인 메뉴 담당
 	}
 	printf("[선택]: ");
 	scanf("%d", &sel);
-	return sel;
+	return sel; // 메인메뉴 선택 반환
 }
 
 void play(int turn, high * p1_H_s, low * p1_L_s, high * p2_H_s, low * p2_L_s) // 턴에 따른 스코어 보드 출력
 {
 	system("mode con cols=128 lines=36");
+	/*주사위가 dice_and_selects 를 호출할 시 바로 주사위가 나오기 때문에 last = 2
+	pan은 1~32 까지 돌면서 화면에 출력하는 변수, 보조 합계 및 합계 subtotal, total*/
 	int last=2, i, pan, p1_subtotal, p2_subtotal, p1_total, p2_total, conti=1;
-	int p1_temp[5] = { 0 }, p2_temp[5] = {0};
-	while (conti <= 24)
+	int p1_temp[5] = { 0 }, p2_temp[5] = {0}; // 플레이어의 주사위 배열
+	while (conti <= 24) // 1 ~ 24 턴 까지
 	{
 		if (turn % 2 == 0) // 플레이어 1
 		{
@@ -576,7 +581,7 @@ void play(int turn, high * p1_H_s, low * p1_L_s, high * p2_H_s, low * p2_L_s) //
 				}
 				else if (pan == 2)
 				{
-					printf("│   %2d / 24   │     your turn   │                │\n", conti);
+					printf("│   %2d / 24   │     your turn   │                │\n", conti); // 현재 진행한 턴 출력
 				}
 				else if (pan == 3 || pan == 5 || pan == 7 || pan == 9 || pan == 11 || pan == 13 || pan == 15 || pan == 17 || pan == 19 || pan == 21 || pan == 23 || pan == 25 || pan == 27 || pan == 29)
 				{
@@ -608,14 +613,15 @@ void play(int turn, high * p1_H_s, low * p1_L_s, high * p2_H_s, low * p2_L_s) //
 				}
 				else if (pan == 16)
 				{
-					for (p1_subtotal = 0, p2_subtotal = 0, i = 0; i < 6; i++)
+					for (p1_subtotal = 0, p2_subtotal = 0, i = 0; i < 6; i++) // 보조 합계를 0으로 다시 초기화 후 사용
 					{
+						// 점수가 쓰인 배열만 보조 합계 합산
 						if (p1_H_s->flag[i] == 'X')
 							p1_subtotal += p1_H_s->arr[i];
 						if (p2_H_s->flag[i] == 'X')
 							p2_subtotal += p2_H_s->arr[i];
 					}
-					if (p1_subtotal >= 63 && p2_subtotal >= 63)
+					if (p1_subtotal >= 63 && p2_subtotal >= 63) // 보조 합계가 63 이상일 경우 +40 점
 					{
 						printf("│    Bonus    │        +40       │       +40       │\n");
 					}
@@ -632,7 +638,7 @@ void play(int turn, high * p1_H_s, low * p1_L_s, high * p2_H_s, low * p2_L_s) //
 							p2_subtotal += BONUS;
 						}
 					}
-					printf("│   Subtotal  │       %2d/63     │      %2d/63     │\n", p1_subtotal, p2_subtotal);
+					printf("│   Subtotal  │       %2d/63     │      %2d/63     │\n", p1_subtotal, p2_subtotal); // 보조 합계 출력
 				}
 				else if (pan == 18)
 				{
@@ -661,16 +667,17 @@ void play(int turn, high * p1_H_s, low * p1_L_s, high * p2_H_s, low * p2_L_s) //
 				}
 				else if (pan == 30)
 				{
-					for (p1_total = 0, p2_total = 0, i = 0; i < 6; i++)
+					for (p1_total = 0, p2_total = 0, i = 0; i < 6; i++) // 합계 출력
 					{
+						// 점수가 쓰인 배열만 보조 합계 합산
 						if (p1_L_s->flag[i] == 'X')
 							p1_total += p1_L_s->arr[i];
 						if (p2_L_s->flag[i] == 'X')
 							p2_total += p2_L_s->arr[i];
 					}
-					p1_total += p1_subtotal;
+					p1_total += p1_subtotal; // 합계에 보조합계 합산
 					p2_total += p2_subtotal;
-					printf("│    total    │        %3d      │       %3d      │\n", p1_total, p2_total);
+					printf("│    total    │        %3d      │       %3d      │\n", p1_total, p2_total); // 합계 출력
 				}
 				else
 				{
@@ -679,15 +686,15 @@ void play(int turn, high * p1_H_s, low * p1_L_s, high * p2_H_s, low * p2_L_s) //
 			}
 			printf("[플레이어 1의 턴]\n");
 			printf("주사위 굴리기를 ");
-			system("pause"); // "주사위 굴리기를 계속하시려면 아무 키나 누르십시오 . . ."
-			dice_and_select(p1_temp, last, p1_H_s, p1_L_s);
-			reset_dice(p1_temp);
-			conti += 1;
+			system("pause"); // "주사위 굴리기를 계속하시려면 아무 키나 누르십시오 . . ." 출력됨.
+			dice_and_select(p1_temp, last, p1_H_s, p1_L_s); // p1 주사위 선택
+			reset_dice(p1_temp); // 플레이어 1의 선택 주사위 초기화
+			conti += 1; // 진행한 턴 1 추가
 		}
-		else // 플레이어 2 [동기화를 위한 복사 대기중]
+		else // 플레이어 2 [플레이어 1]과 동일]
 		{
 			system("cls");
-			for (pan = 0; pan < 32; pan++) // 현재 스코어 작업 대기중
+			for (pan = 0; pan < 32; pan++) 
 			{
 				if (pan == 0)
 				{
@@ -802,9 +809,9 @@ void play(int turn, high * p1_H_s, low * p1_L_s, high * p2_H_s, low * p2_L_s) //
 			}
 			printf("[플레이어 2의 턴]\n");
 			printf("주사위 굴리기를 ");
-			system("pause"); // "주사위 굴리기를 계속하시려면 아무 키나 누르십시오 . . ."
-			dice_and_select(p2_temp, last, p2_H_s, p2_L_s);
-			reset_dice(p2_temp);
+			system("pause");
+			dice_and_select(p2_temp, last, p2_H_s, p2_L_s); // p2 주사위 선택
+			reset_dice(p2_temp); // 플레이어2 의 선택 주사위 초기화
 			conti += 1;
 		}
 		turn += 1;
